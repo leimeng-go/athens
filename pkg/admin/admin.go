eecdd1f06452bc899b969e81a6b2b8899c4214de
leimeng <1955854667@qq.com>
2025-07-23 00:22:05 +0800

    feat: 添加HTML响应

--- pkg/admin/admin.go
+++ pkg/admin/admin.go
@@ -2,13 +2,27 @@ package admin
 
 import(
 	"net/http"
-
+	"os"
 )
 
 const Admin="/admin"
 
 func AdminHandler() http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("admin"))
+		// 设置内容类型为HTML
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		
+		// 读取HTML模板文件
+		templatePath := "static/nexus-admin-template.html"
+		html, err := os.ReadFile(templatePath)
+		if err != nil {
+			// 如果模板文件不存在，返回错误信息
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error loading admin template: " + err.Error()))
+			return
+		}
+		
+		// 返回HTML内容
+		w.Write(html)
     })
 }
